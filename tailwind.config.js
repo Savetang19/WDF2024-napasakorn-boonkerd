198200d92cba533e326ad9a0fc56722d0a1477a1
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2024-10-08 15:31:33 +0200

    feat: add theme to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./views/**/*.handlebars"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'champagne' : "#F5E6CA",
+        'delft-blue': "#343F56",
+        'redwood': "#B0413E",
+        'viridian': "#668F80",
+        'periwinkle': "#A7ACD9"
+      },
+      fontFamily : {
+        'primary': ['Rubik', ...defaultTheme.fontFamily.sans],
+        'body': ['"Noto Sans"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
