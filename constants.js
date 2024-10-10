fba61dac8f1d8cf4485b0d82a7704fcca0ae420d
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2024-10-10 04:14:28 +0200

    feat: add cover_url for songs constant

--- constants.js
+++ constants.js
@@ -7,18 +7,18 @@ const users = [
 ]
 
 const songs = [
-    { 'sid': 1, 'title': 'Bohemian Rhapsody', 'artist': 'Queen', 'album': 'A Night at the Opera', 'genre': 'Rock', 'release_year': 1975 },
-    { 'sid': 2, 'title': 'Killer Queen', 'artist': 'Queen', 'album': 'Sheer Heart Attack', 'genre': 'Rock', 'release_year': 1974 },
-    { 'sid': 3, 'title': 'Thunderstruck', 'artist': 'AC/DC', 'album': 'The Razors Edge', 'genre': 'Rock', 'release_year': 1990 },
-    { 'sid': 4, 'title': 'Back in Black', 'artist': 'AC/DC', 'album': 'Back in Black', 'genre': 'Rock', 'release_year': 1980 },
-    { 'sid': 5, 'title': `Sweet Child O' Mine`, 'artist': 'Guns N Roses', 'album': 'Appetite for Destruction', 'genre': 'Rock', 'release_year': 1987 },
-    { 'sid': 6, 'title': 'Welcome To The Jungle', 'artist': 'Guns N Roses', 'album': 'Appetite for Destruction', 'genre': 'Rock', 'release_year': 1987 },
-    { 'sid': 7, 'title': 'November Rain', 'artist': 'Guns N Roses', 'album': 'Use Your Illusion I', 'genre': 'Rock', 'release_year': 1991 },
-    { 'sid': 8, 'title': 'Rap God', 'artist': 'Eminem', 'album': 'The Marshall Mathers LP2', 'genre': 'Hip Hop', 'release_year': 2013 },
-    { 'sid': 9, 'title': 'My Name Is', 'artist': 'Eminem', 'album': 'The Slim Shady LP', 'genre': 'Hip Hop', 'release_year': 1999 },
-    { 'sid': 10, 'title': 'Godzilla', 'artist': 'Eminem', 'album': 'Music to Be Murdered By', 'genre': 'Hip Hop', 'release_year': 2020 },
-    { 'sid': 11, 'title': 'Throne', 'artist': 'Bring Me The Horizon', 'album': `That's the Spirit`, 'genre': 'Rock', 'release_year': 2015 },
-    { 'sid': 12, 'title': 'Teardrops', 'artist': 'Bring Me The Horizon', 'album': 'Post Human: Survival Horror', 'genre': 'Rock', 'release_year': 2020 },
+    { 'sid': 1, 'title': 'Bohemian Rhapsody', 'artist': 'Queen', 'album': 'A Night at the Opera', 'genre': 'Rock', 'release_year': 1975, 'cover_url': '/img/bohemian-rhapsody.png' },
+    { 'sid': 2, 'title': 'Killer Queen', 'artist': 'Queen', 'album': 'Sheer Heart Attack', 'genre': 'Rock', 'release_year': 1974, 'cover_url': '/img/killer-queen.png' },
+    { 'sid': 3, 'title': 'Thunderstruck', 'artist': 'AC/DC', 'album': 'The Razors Edge', 'genre': 'Rock', 'release_year': 1990, 'cover_url': '/img/thunderstruck.png' },
+    { 'sid': 4, 'title': 'Back in Black', 'artist': 'AC/DC', 'album': 'Back in Black', 'genre': 'Rock', 'release_year': 1980, 'cover_url': '/img/back-in-black.png' },
+    { 'sid': 5, 'title': `Sweet Child O' Mine`, 'artist': 'Guns N Roses', 'album': 'Appetite for Destruction', 'genre': 'Rock', 'release_year': 1987, 'cover_url': '/img/sweet-child-o-mine.png' },
+    { 'sid': 6, 'title': 'Welcome To The Jungle', 'artist': 'Guns N Roses', 'album': 'Appetite for Destruction', 'genre': 'Rock', 'release_year': 1987, 'cover_url': '/img/welcome-to-the-jungle.png' },
+    { 'sid': 7, 'title': 'November Rain', 'artist': 'Guns N Roses', 'album': 'Use Your Illusion I', 'genre': 'Rock', 'release_year': 1991, 'cover_url': '/img/november-rain.png' },
+    { 'sid': 8, 'title': 'Rap God', 'artist': 'Eminem', 'album': 'The Marshall Mathers LP2', 'genre': 'Hip Hop', 'release_year': 2013, 'cover_url': '/img/rap-god.png' },
+    { 'sid': 9, 'title': 'My Name Is', 'artist': 'Eminem', 'album': 'The Slim Shady LP', 'genre': 'Hip Hop', 'release_year': 1999, 'cover_url': '/img/my-name-is.png' },
+    { 'sid': 10, 'title': 'Godzilla', 'artist': 'Eminem', 'album': 'Music to Be Murdered By', 'genre': 'Hip Hop', 'release_year': 2020, 'cover_url': '/img/godzilla.png' },
+    { 'sid': 11, 'title': 'Throne', 'artist': 'Bring Me The Horizon', 'album': `That's the Spirit`, 'genre': 'Rock', 'release_year': 2015, 'cover_url': '/img/throne.png' },
+    { 'sid': 12, 'title': 'Teardrops', 'artist': 'Bring Me The Horizon', 'album': 'Post Human: Survival Horror', 'genre': 'Rock', 'release_year': 2020, 'cover_url': '/img/teardrops.png' },
 ]
 
 const reviews = [
